f2ece25e63f3fcacd6c4d47322995f74b9f68d71
GREATWARE <raniem57@gmail.com>
2024-04-13 15:48:00 -0700

    adjusted the media query to 700px

--- src/pageComponents/Homepage/ServiceBox.jsx
+++ src/pageComponents/Homepage/ServiceBox.jsx
@@ -4,7 +4,7 @@ export const ServiceBox = ({ServiceTitle, list, ServiceBox}) => {
     <div className="ServiceBox-Wrapper flex flex-col h-fit w-full justify-between " id={ServiceBox}>
         <div className="Img h-64 w-full bg-cover bg-center bg-no-repeat"></div>
         <div className="Description pt-4 pb-2 flex-grow px-5 flex flex-col h-44 bg-white justify-start text-slate-800 border-b-2 border-purple-900">
-            <p className=" font-semibold text-wrap">{ServiceTitle}</p>
+            <p className=" font-semibold text-2xl text-wrap">{ServiceTitle}</p>
             <u className=" lg:text-sm text-base no-underline mt-2">
              {list.map((list, index) => (
             <li key={index} className="serviceFeatures">{list}</li>
